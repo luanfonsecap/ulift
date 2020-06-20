30540b7079ccb54c9cd09967363dd687f6848119
Luan Fonseca <luanfonsecap@icloud.com>
2020-06-20 12:13:07 -0300

    add yup validation to create a new user

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,10 +1,28 @@
 import { Request, Response } from 'express';
+import * as Yup from 'yup';
 
 import CreateUserService from '../services/CreateUserService';
+import YupValidationError from '../errors/YupValidationError';
 
 class UsersController {
 	async store(req: Request, res: Response): Promise<Response> {
 		const { username, email, password, defaultDestination } = req.body;
+
+		const schema = Yup.object().shape({
+			username: Yup.string().required('Username is required'),
+			email: Yup.string()
+				.email('Inser a valid e-mail')
+				.required('E-mail is required'),
+			password: Yup.string()
+				.required('Password is required')
+				.min(6, 'The password must be at least six characters long'),
+		});
+
+		try {
+			await schema.validate({ username, email, password, defaultDestination });
+		} catch (error) {
+			throw new YupValidationError(error.message);
+		}
 
 		const createUserService = new CreateUserService();
 
