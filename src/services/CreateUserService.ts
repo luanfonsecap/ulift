aef876c196dac9bb12ab168b6dfa9578c0752b7a
Luan Fonseca <luanfonsecap@icloud.com>
2020-06-20 11:20:26 -0300

    created CreateUserService

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,25 @@
+import { Document } from 'mongoose';
+import UserRepository from '../repositories/UserRepostiroy';
+import UserDTO from '../repositories/interfaces/dtos/ICreateUserDTO';
+
+class CreateUserService {
+	public async execute({
+		username,
+		password,
+		email,
+		defaultDestination,
+	}: UserDTO): Promise<Document> {
+		const userRepository = new UserRepository();
+
+		const user = await userRepository.create({
+			username,
+			password,
+			email,
+			defaultDestination,
+		});
+
+		return user;
+	}
+}
+
+export default CreateUserService;
