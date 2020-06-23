473f3794644798de63491aa80eec8ef5fc13006a
Luan Fonseca <luanfonsecap@icloud.com>
2020-06-22 22:28:56 -0300

    register user controller update on routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -5,5 +5,6 @@ import UsersController from '../controllers/UsersController';
 const usersRouter = Router();
 
 usersRouter.post('/', UsersController.store);
+usersRouter.post('/:id', UsersController.update);
 
 export default usersRouter;
